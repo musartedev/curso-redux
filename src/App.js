91349d534e7742b7b6adfc4acc6c92c4daaebe93
Mariangelica Useche <mariangelica.useche@gmail.com>
2022-05-05 13:48:10 +0200

    Add redux hooks

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import { useEffect, useState } from 'react';
-import { connect } from 'react-redux';
+import { useEffect } from 'react';
 import { Col } from 'antd';
+import { useDispatch, useSelector } from 'react-redux';
 import Searcher from './components/Searcher';
 import PokemonList from './components/PokemonList';
 import { getPokemon } from './api';
-import { setPokemons as setPokemonsActions } from './actions';
+import { setPokemons } from './actions';
 import logo from './statics/logo.svg';
 import './App.css';
 
-function App({ pokemons, setPokemons }) {
-  console.log("🚀 ~ file: App.js ~ line 12 ~ App ~ pokemons", pokemons)
+function App() {
+  const pokemons = useSelector((state) => state.pokemons);
+  const dispatch = useDispatch();
+
   useEffect(() => {
     const fetchPokemons = async () => {
       const pokemonsRes = await getPokemon();
-      setPokemons(pokemonsRes);
+      dispatch(setPokemons(pokemonsRes));
     };
 
     fetchPokemons();
@@ -32,12 +34,4 @@ function App({ pokemons, setPokemons }) {
   );
 }
 
-const mapStateToProps = (state) => ({
-  pokemons: state.pokemons,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  setPokemons: (value) => dispatch(setPokemonsActions(value)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
