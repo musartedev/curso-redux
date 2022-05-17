f82cb9d13212e70acd7e614976f65006c18d47ca
Mariangelica Useche <mariangelica.useche@gmail.com>
2022-05-17 17:21:20 +0200

    Add dataSlice

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,10 +1,8 @@
-import { combineReducers } from 'redux-immutable';
-import { pokemonsReducer } from './pokemons';
-import { uiReducer } from './ui';
+import { combineReducers } from 'redux';
+import dataReducer from '../slices/dataSlice';
 
 const rootReducer = combineReducers({
-  data: pokemonsReducer,
-  ui: uiReducer,
+  data: dataReducer,
 });
 
 export default rootReducer;
---
+++ src/slices/dataSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  pokemons: [],
+};
+
+export const dataSlice = createSlice({
+  name: 'data',
+  initialState,
+  reducers: {
+    setPokemons: (state, action) => {
+      state.pokemons = action.payload;
+    },
+    setFavorite: (state, action) => {
+      const currentPokemonIndex = state.pokemons.findIndex((pokemon) => {
+        return pokemon.id === action.payload.pokemonId;
+      });
+
+      if (currentPokemonIndex >= 0) {
+        const isFavorite = state.pokemons[currentPokemonIndex].favorite;
+
+        state.pokemons[currentPokemonIndex].favorite = !isFavorite;
+      }
+    },
+  },
+});
+
+export const { setFavorite, setPokemons } = dataSlice.actions;
+console.log('🚀 ~ file: dataSlice.js ~ line 29 ~ dataSlice', dataSlice);
+
+export default dataSlice.reducer;
